133330b3e8891326f686c9af6cacd4f1de4522ff
Nova2419 <shivamojha2419@gmail.com>
2023-09-22 01:03:52 +0530

    Project Upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='pt-10 mb-5'>
+      <p>Made with ♥ Shivam Ojha</p>
+    </footer>
+  );
+};
+
+export default Footer;
