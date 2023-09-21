c406f5dd6c00295df070f0dec041734c109e4121
Nova2419 <shivamojha2419@gmail.com>
2023-09-22 01:07:06 +0530

    Github Link Added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <button
           type="button"
           onClick={() =>
-            window.open("")
+            window.open("https://github.com/Nova2419/AI_Summerizer_Sumupzz#ai-summarizer-sumz")
           }
           className="black_btn"
         >
